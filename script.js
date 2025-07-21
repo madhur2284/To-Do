7e170206d48fd55aca281457349856fbfe031a3d
Madhur <debugginglife28@gmail.com>
2025-07-21 21:10:48 +0530

    Adding New Feature And Structure The Code

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 let count = 3;
 function addToDo(){
+    //Taking value form input
     const inputEl = document.querySelector("input");
-    const el = document.createElement("div");
-    el.innerHTML = '<div id="ToDo-' + count + '" class="ToDo"><h4 id="text' + count + '">' + inputEl.value + '</h4><button onclick="Delete(' + count +')">Delete</button><button onclick="Completed('+ count +')">Done!</button></div>';
+    //Validating the input
+    if(inputEl.value == ""){
+        alert("Please Enter your Task");
+        return;
+    }
     const parent = document.querySelector("#TODOs");
-    parent.appendChild(el);
+    //creating main div which will append to parent
+    const el1 = document.createElement("div");
+    el1.setAttribute("id", "ToDo-" + count);
+    el1.setAttribute("class", "ToDo");
+    //creating an h4 tag which will have the inputEl.value
+    const el2 = document.createElement("h4");
+    el2.textContent = inputEl.value;
+    el2.setAttribute("id", "text" + count);
+    //creating Done Button
+    const CompleteEl = document.createElement("button");
+    CompleteEl.textContent = "Done!";
+    CompleteEl.setAttribute("onclick", "Completed(" + count + ")");
+    //Creating Delete Button
+    const DeleteEl = document.createElement("button");
+    DeleteEl.textContent = "Delete";
+    DeleteEl.setAttribute("onclick", "Delete(" + count + ")");
+    //Appending el2, CompleteEl, DeleteEl into el1
+    el1.appendChild(el2);
+    el1.appendChild(DeleteEl);
+    el1.appendChild(CompleteEl);
+    //Appending el1 into parent
+    parent.appendChild(el1);
+    //input bar  will get empty when this function will called
+    inputEl.value = "";
     count++;
 }
 
